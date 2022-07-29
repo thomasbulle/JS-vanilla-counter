73147b46a330ee7d37486e17aafa4673c6fed326
Thomas Bulle <41519508+thomasbulle@users.noreply.github.com>
2022-07-29 11:39:10 +0200

    update the js file to add the solution

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+const handleClickLess = () => {
+  const total = document.getElementById('total');
+  total.textContent = (parseInt(total.textContent) - 1).toString();
+}
+
+const handleClickReset = () => {
+  const total = document.getElementById('total');
+  total.textContent = '0';
+}
+
+const handleClickAdd = () => {
+  const total = document.getElementById('total');
+  total.textContent = (parseInt(total.textContent) + 1).toString();
+}
+
 const init = () => {
-  // Your code here ...
+  const lessButton = document.getElementById('less-button');
+  lessButton.addEventListener('click', handleClickLess);
+
+  const resetButton = document.getElementById('reset-button');
+  resetButton.addEventListener('click', handleClickReset);
+
+  const addButton = document.getElementById('add-button');
+  addButton.addEventListener('click', handleClickAdd);
 }
 
 window.onload = init;
